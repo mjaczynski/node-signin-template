27610c7b5382fdf3af22b13901eef03e4630a53b
Michel Jaczynski <mjaczynski@gmail.com>
2015-07-13 21:00:12 -0700

    added bower install

--- config/env/openshift.js
+++ config/env/openshift.js
@@ -23,6 +23,5 @@ module.exports.configure  = function (config) {
 
     config.db = 'mongodb://' + process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' + process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/' + process.env.OPENSHIFT_APP_NAME;
     return config;
-
 };
 
