6cfc1deebe85c3cf213c1682761e03b7df3f5d28
Michel Jaczynski <mjaczynski@gmail.com>
2015-07-22 10:32:12 -0700

    updated local config

--- config/env/local.js
+++ config/env/local.js
@@ -4,11 +4,7 @@ var winston = require('winston');
 
 module.exports.configure = function (config) {
     winston.info('Configuring local deployment');
-    config.email = {
-        user : "mjaczynski",
-        key : "Jac70sge",
-        from : "mycloudtips@gmail.com"
-    };
+    
     return config;
 
 };
